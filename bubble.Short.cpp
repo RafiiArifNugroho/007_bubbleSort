7ca68ecc1b4aca687dd5cfba7009002eaf171c8f
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-03-13 11:57:07 +0700

    Prosedur bubble.Short

--- bubble.Short.cpp
+++ bubble.Short.cpp
@@ -4,3 +4,48 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array: ";
+        cin >> n;          //input dari pengguna
+        if (n<=20)
+            break;
+        else{
+            cout << "\nArray dapat mempunyai maksimal 20 element\n" << endl;
+        } 
+    }
+
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan element array" << endl;
+    cout << "======================" << endl;
+
+    for (int i - 0; i <n; i++){
+        cout << "Data ke-" << (i+1) << ";";
+        cin >> arr[i];
+    }
+
+}
+
+
+void bubbleSortArray(){
+    int pass = 1; //step1
+
+   do 
+   {
+    for (int j = 0; j <= n -1-pass;j++)
+    {//step 2
+
+        if(arr[j] > arr[j+1]){
+            int temp;
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+
+    pass = pass + 1; //step4
+    
+   } while (pass <= n - 1); //step 5
+}
